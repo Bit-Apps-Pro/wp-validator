e1212276988dfe149646f826a9895041e4d13484
csemazharul <csemazharulislam@gmail.com>
2023-09-25 18:50:06 +0600

    feat: added max rue

--- src/Rules/MaxRule.php
+++ src/Rules/MaxRule.php
@@ -1,10 +1,27 @@
 <?php
 namespace BitApps\WPValidator\Rules;
 
-class MaxRule
+use BitApps\WPValidator\Rule;
+
+class MaxRule extends Rule
 {
-    public static function validate($field, $value, $max)
+    protected $message = "The :attribute must not exceed a specified maximum :max.";
+
+    protected $params = ['max'];
+
+    public function validate($value, $field = null, $params)
     {
+        $max = $params[0];
         return strlen($value) <= $max;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    public function message()
+    {
+        return $this->message;
     }
 }
