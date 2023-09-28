223580e98b6c0afffc50777299bac5da78bf5c5a
Md Mazharul Islam <csemazharulislam@gmail.com>
2023-09-28 22:34:10 +0600

    feat: added data container class

---
+++ src/InputDataContainer.php
@@ -0,0 +1,48 @@
+<?php
+namespace BitApps\WPValidator;
+
+class InputDataContainer
+{
+    private $data = []; 
+    private $attributeKey;
+    private $attributeLabel;
+
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function setAttributeKey($key)
+    {
+        $this->attributeKey = $key;
+    }
+
+    public function getAttributeKey()
+    {
+        return $this->attributeKey;
+    }
+
+    public function getAttributeValue()
+    {
+        if(isset($this->data[$this->attributeKey])){
+            return $this->data[$this->attributeKey];
+        }
+        return null;
+    }
+
+    public function setAttributeLabel($value)
+    {
+        $this->attributeLabel = $value;
+    }
+
+    public function getAttributeLabel()
+    {
+        return $this->attributeLabel;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+}
