40a664ed31fda62c31328295c8925effe5b18e31
Md Mazharul Islam <csemazharulislam@gmail.com>
2023-09-28 22:32:12 +0600

    feat: added index file for testing

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
 
-use BitApps\ValidatorSanitizer\Validator;
+use BitApps\WPValidator\Validator;
 
 require 'vendor/autoload.php';
+//basic testing 
+$inputs = [
+    'name' => '',
+    'email' => 'invalidmail',
+    'age' => 'ass',
+];
+
+$rules = [
+    'name' => ['required'],
+    'email' => ['nullable', 'email'],
+    'age' => ['nullable', 'integer'],
+];
+
+$customMessages=[
+    'email'=>[
+        'email'=>'This :value is invalid '
+    ]
+];
+
+$attributeLabels=[
+    'name'=>'Name',
+    'age'=>'Age',
+    'email'=>'Email'
+];
 
 $validator = new Validator;
+
+$validator->make($inputs, $rules, $customMessages, $attributeLabels);
+
+echo "<pre>";
+echo print_r($validator->errors(), true);
+echo "<pre>";
+
