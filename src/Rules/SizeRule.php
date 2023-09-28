9a7fecde7bca17c302644a01c79677f4c04b1ccb
Md Mazharul Islam <csemazharulislam@gmail.com>
2023-09-29 01:22:10 +0600

    feat: added new size rule

---
+++ src/Rules/SizeRule.php
@@ -0,0 +1,39 @@
+<?php
+namespace BitApps\WPValidator\Rules;
+
+use BitApps\WPValidator\Rule;
+
+class SizeRule extends Rule
+{
+
+    private $message = "The :attribute field must be :size characters";
+
+    protected $requireParameters = ['size'];
+
+    public function validate($value)
+    {
+        $this->checkRequiredParameter($this->requireParameters);
+
+        $size = $this->getParameter('size');
+
+        if (is_string($value)) {
+            return strlen($value) == $size;
+        } elseif (is_int($value)) {
+            return $value === $size;
+        } else if (is_array($value)) {
+            return count($value) === $size;
+        } else {
+            return false;
+        }
+    }
+
+    public function getParamKeys()
+    {
+        return $this->requireParameters;
+    }
+
+    public function message()
+    {
+        return $this->message;
+    }
+}
