7bcd3f2681f440f12f9de8d063001216f3cf357c
csemazharul <csemazharulislam@gmail.com>
2023-09-30 13:15:30 +0600

    test: updated rule test case

--- tests/Rules/DigitsTest.php
+++ tests/Rules/DigitsTest.php
@@ -6,8 +6,12 @@ test('digit', function () {
 
     $rule = new DigitsRule();
 
+    $paramKeys = ['digits'];
+    $paramValues = [5];
+
+    $rule->setParameterValues($paramKeys, $paramValues);
+
     expect(true)->toBe($rule->validate('23431'));
-    expect(true)->toBe($rule->validate(2323));
-    expect(true)->toBe($rule->validate('2323'));
+    expect(true)->toBe($rule->validate(23232));
 
 });
