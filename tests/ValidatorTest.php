09b91f15e4b44015d8dccf8e8122c200e7031e75
Md Mazharul Islam <csemazharulislam@gmail.com>
2023-10-01 01:57:10 +0600

    feat: added validator test

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -3,19 +3,43 @@
 use BitApps\WPValidator\Validator;
 
 test('validator', function () {
-    $inputs = [
-        'name' => 'Fabien',
-        'email' => 'test@email.tld',
-        'age' => 21,
+    $validator = new Validator;
+
+    $data = [
+        'first_name' => 'John',
+        'last_name' => '',
+        'email' => 'email@example',
+        'password' => '##112233',
+        'confirm_password' => '##11223',
     ];
 
     $rules = [
-        'name' => ['required'],
-        'email' => ['required'],
-        'age' => ['required'],
+        'first_name' => ['required', 'string'],
+        'last_name' => ['required', 'string'],
+        'email' => ['required', 'email'],
+        'password' => ['required', 'min:8'],
+        'confirm_password' => ['required', 'same:password'],
     ];
 
-    $validator = new Validator;
-    $validator->make($inputs, $rules);
+    $customMessages = [];
 
+    $attributes = [
+        'first_name' => 'First Name',
+        'last_name' => 'Last Name',
+        'email' => 'Email',
+        'password' => 'B',
+        'confirm_password' => 'Confirm Password',
+    ];
+
+    $validation = $validator->validate($data, $rules, $customMessages, $attributes);
+    $errors = $validation->errors();
+    expect(true)->toBe($validation->fails(true));
+    expect($errors)->toBeArray();
+    expect($errors)->toHaveCount(3);
+    expect($errors)->toHaveKeys(['last_name', 'email', 'confirm_password']);
+    expect($errors)->toBe([
+        'last_name' => ['The Last Name field is required'],
+        'email' => ['The Email must be a valid email address'],
+        'confirm_password' => ['The Confirm Password and password must match'],
+    ]);
 });
