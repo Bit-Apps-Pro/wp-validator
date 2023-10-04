dd350b28dc3ec9a7743c0ea97e01d836876f005e
csemazharul <csemazharulislam@gmail.com>
2023-10-04 13:12:18 +0600

    fix: check also numeric value in helper function

--- src/Rules/RequiredRule.php
+++ src/Rules/RequiredRule.php
@@ -1,15 +1,18 @@
 <?php
 namespace BitApps\WPValidator\Rules;
 
+use BitApps\WPValidator\Helpers;
 use BitApps\WPValidator\Rule;
 
 class RequiredRule extends Rule
 {
+    use Helpers;
+
     private $message = 'The :attribute field is required';
 
     public function validate($value)
     {
-        return !empty($value);
+        return !$this->isEmpty($value);
     }
 
     public function message()
