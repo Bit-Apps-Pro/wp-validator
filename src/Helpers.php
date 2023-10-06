961202ec0b2a6d8ea605e3fc8706e8f18e074fe3
Md Mazharul Islam <csemazharulislam@gmail.com>
2023-10-07 01:07:48 +0600

    feat: added get value length helper function

--- src/Helpers.php
+++ src/Helpers.php
@@ -4,12 +4,28 @@ namespace BitApps\WPValidator;
 
 trait Helpers
 {
-    public function isEmpty($val)
+    protected function isEmpty($val)
     {
         if (empty($val) && !is_numeric($val)) {
             return true;
         }
 
         return false;
+    }
+
+    protected function getValueLength($value)
+    {
+        if (is_int($value) || is_float($value)) {
+            $value = $value;
+        } elseif (is_string($value)) {
+            $value = mb_strlen($value, 'UTF-8');
+        } elseif (is_array($value)) {
+            $value = count($value);
+        } else {
+            return false;
+        }
+
+        return $value;
+
     }
 }
