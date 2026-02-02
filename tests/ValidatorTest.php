3e1fe0ccf3b7880df18d1271ad251b0edc2144d6
csemazharul <csemazharulislam@gmail.com>
2026-02-02 12:37:48 +0600

    test: updated test case

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -6,50 +6,52 @@ test('validator', function () {
     $validator = new Validator;
 
     $data = [
-        'names' => [
+        'names'            => [
             'first_name' => 'John',
-            'last_name' => '',
+            'last_name'  => '',
         ],
-        'phone' => '018xxxxxxxx',
-        'age' => '21',
-        'email' => 'email@example',
-        'password' => '##112233',
+        'nickname'         => '',
+        'phone'            => '018xxxxxxxx',
+        'age'              => '21',
+        'email'            => 'email@example',
+        'password'         => '##112233',
         'confirm_password' => '##11223',
-        'agree' => 'yes',
+        'agree'            => 'yes',
     ];
 
     $rules = [
         'names.first_name' => ['required', 'string'],
-        'names.last_name' => ['required', 'string'],
-        'age' => ['required', 'integer', 'between:18,25'],
-        'phone' => ['required', 'size:11'],
-        'email' => ['required', 'email'],
-        'password' => ['required', 'min:8'],
+        'names.last_name'  => ['required', 'string'],
+        'nickname'         => ['present'],
+        'age'              => ['required', 'integer', 'between:18,25'],
+        'phone'            => ['required', 'size:11'],
+        'email'            => ['required', 'email'],
+        'password'         => ['required', 'min:8'],
         'confirm_password' => ['required', 'same:password'],
-        'agree' => ['required', 'accepted'],
+        'agree'            => ['required', 'accepted'],
     ];
 
     $customMessages = [];
 
     $attributes = [
-        'first_name' => 'First Name',
-        'names.last_name' => 'Last Name',
-        'phone' => 'Phone',
-        'email' => 'Email',
-        'password' => 'Password',
+        'first_name'       => 'First Name',
+        'names.last_name'  => 'Last Name',
+        'phone'            => 'Phone',
+        'email'            => 'Email',
+        'password'         => 'Password',
         'confirm_password' => 'Confirm Password',
     ];
 
     $validation = $validator->make($data, $rules, $customMessages, $attributes);
-    $errors = $validation->errors();
+    $errors     = $validation->errors();
     expect(true)->toBe($validation->fails(true));
     expect($errors)->toBeArray();
     expect($errors)->toHaveCount(4);
     expect($errors)->toHaveKeys(['names.last_name', 'age', 'email', 'confirm_password']);
     expect($errors)->toBe([
-        'names.last_name' => ['The Last Name field is required'],
-        'age' => ['The age must be between 18 and 25'],
-        'email' => ['The Email must be a valid email address'],
+        'names.last_name'  => ['The Last Name field is required'],
+        'age'              => ['The age must be between 18 and 25'],
+        'email'            => ['The Email must be a valid email address'],
         'confirm_password' => ['The Confirm Password and password must match'],
     ]);
 });
